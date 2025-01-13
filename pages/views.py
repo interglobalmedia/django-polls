69b38e30a0e5aa3e3ddcdf458af6cfc1b1b8496a
Maria Campbell <interglobalmedia@gmail.com>
2025-01-13 07:28:49 -0500

    Add pages/ app

---
+++ pages/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import HomePage
+
+# Create your views here.
+def index(request):
+    pages = HomePage.objects.all()
+    return render(request, 'pages/index.html', {"pages": pages})
