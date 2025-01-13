33978e45e7956dc6e4f468b04a27bb09c2261d29
Maria Campbell <interglobalmedia@gmail.com>
2025-01-13 12:13:52 -0500

    Add lint formatting

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'pages'
+app_name = "pages"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
 ]
