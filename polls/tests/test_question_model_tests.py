d7dcc26c8aea5b40e94c1e1352dbde58777f7359
Maria Campbell <interglobalmedia@gmail.com>
2025-01-13 12:18:29 -0500

    Add lint formatting

--- polls/tests/test_question_model_tests.py
+++ polls/tests/test_question_model_tests.py
@@ -1,8 +1,8 @@
 import datetime
 
-from django.utils import timezone
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
 from ..models import Question
 
@@ -104,14 +104,15 @@ class QuestionIndexViewTests(TestCase):
             [question2, question1],
         )
 
+
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
         The detail view of a question with a pub_date in the future
         returns a 404 not found.
         """
-        future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -120,7 +121,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
-        past_question = create_question(question_text='Past Question.', days=-5)
-        url = reverse('polls:detail', args=(past_question.id,))
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
