bdb36cded2d810e51540e16746db0f3530f3d7a7
Maria Campbell <interglobalmedia@gmail.com>
2025-01-15 21:34:41 -0500

    Add lint format changes

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+import datetime
+
+from django.contrib import admin  # new
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")  # human readable name
+
+    @admin.display(  # new
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def recently_published(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
